fdadfb4023af5a623af43e23be1ac2a5d8390be2
MariaZamora <mariaz09@gmail.com>
2020-10-03 01:42:13 -0400

    Focusing on anual avg temp per state

--- anual_state.R
+++ anual_state.R
@@ -1,39 +1,37 @@
 #Anual per state
 
-library(tidyverse)
-library(ggplot2)
-library(gganimate)
+source("DataProc.R")
 
 
-
-years_int <- c(1985:2019) #set the relevant years for files
-
-# create file paths/names
-file_names <-paste("Mex Temp/", years_int, "Tmed.xlsx", sep = "")
-
-
-#d85<- readxl::read_xlsx("Mex Temp/1985Tmed.xlsx", skip = 1, 
-#                        col_types = c("text", "numeric","numeric","numeric","numeric","numeric","numeric","numeric",
-#                                     "numeric","numeric","numeric","numeric","numeric","numeric")
-#                         )
-
-
-#read in xlsx files. Results in a list of dataframes
-list_dfs <- lapply(file_names, readxl::read_xlsx, 
-                   skip = 1,
-                   col_types = c("text", "numeric","numeric","numeric","numeric","numeric","numeric","numeric",
-                                 "numeric","numeric","numeric","numeric","numeric","numeric")
-)
-
-
-#Name each element in the list (year)
-names(list_dfs) <- paste(years_int)
-
-#unnest/bind data frames
-aTemp <- bind_rows(list_dfs, .id = "year")
-
-#Tidy data, need to result in a long df of state, year, month, temp
-aTemp <- aTemp %>% 
-  gather(key = "month", value = "temperature", ENE:ANUAL) %>% 
-  mutate(year = as.integer(year(as.Date.character(year, "%Y"))) 
+# keep only annual data (drop monthly)
+aTemp <- aTemp %>%
+  filter(fmeses == "ANUAL") %>% 
+  mutate(
+    round_diff = round(diff, digits = 3),
+    round_temp = round(temperature, digits = 2)
   )
+
+# base for animation
+p <-
+  ggplot(aTemp, aes(x = fest, y = 1, color = as.factor(binned_diff)))+
+  geom_point(size = 20) +
+  geom_text(aes(label = as.character(round_temp)), color = "black")+
+  scale_color_brewer(palette = "Spectral", direction = -1) +
+  facet_wrap(vars(reg_state), ncol = 5, nrow = 7, scales = "free_x")+
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    legend.position = "bottom"
+    )+
+  guides(color = "none") +
+  labs( title = "Temperatura promedio anual por entidad entre 1985-2019",
+        subtitle = "Year: {frame_time}",
+        caption = "Colores representan desviaciones estandard de la temperatura promedio entre 1984-1999"
+        # ,
+        # color = "DS de promedio"
+  ) +
+  transition_time(year)
+
+animate(p, fps = 3, width =450, height = 400)
+anim_save("Anual_1.gif")
